c453bbac013a8f1afede5c42d5282abeac6587d5
ahmedBou <instinctauto@gmail.com>
2020-11-05 15:28:54 +0100

    binding to custom event

--- cmp-databiding/src/app/cockpit/cockpit.component.ts
+++ cmp-databiding/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,27 +8,24 @@ import { Component, OnInit } from '@angular/core';
 
 
 export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   newServerName = '';
   newServerContent = '';
-  
-  
+
   constructor() { }
 
-
-
   onAddServer() {
-    // this.serverElements.push({
-    //   type: 'server',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });    
   }
   onAddBlueprint() {
-    // this.serverElements.push({
-    //   type: 'blueprint',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+    this.blueprintCreated.emit({
+    serverName: this.newServerName,
+    serverContent: this.newServerContent
+    });   
   }
 
   ngOnInit(): void {
